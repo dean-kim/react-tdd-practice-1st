1bb29f013bd2b370f0d24d843d8d955a621e2d97
dean-kim <keunu07@gmail.com>
2018-10-10 14:54:06 +0900

    test about promotion checkbox defalut true

--- tdd-react/src/Form.test.js
+++ tdd-react/src/Form.test.js
@@ -5,4 +5,17 @@ import Adapter from 'enzyme-adapter-react-16'
 import toJson from 'enzyme-to-json'
 import api from './api'
 
+
 configure({ adapter: new Adapter() })
+
+describe('<Form /', () => {
+    // opted in by default
+    test('receive promotions default is true', () => {
+        const wrapper = shallow(<Form />)
+        const promotionInput = wrapper.find('[data-testid="checked"]')
+        expect(promotionInput.props().checked).toBe(true)
+    })
+    // actually input their information
+    // submit the form, calls api
+    // matches snapshot
+})

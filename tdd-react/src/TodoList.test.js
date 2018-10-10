7da4c5df2ad01e3d87d4992ffcb1ea568424c600
dean-kim <keunu07@gmail.com>
2018-10-10 11:12:58 +0900

    test action creater addTodo

--- tdd-react/src/TodoList.test.js
+++ tdd-react/src/TodoList.test.js
@@ -11,3 +11,16 @@ configure({ adapter: new Adapter() })
 // removeTodo with li click
 // matches snapshot
 
+describe('<TodoList />', () => {
+    it('calls addTodo Redux action creater with button click', () => {
+        const props = {
+            addTodo: jest.fn(),
+            todos: []
+        }
+        const wrapper = shallow(<TodoList {...props} />)
+        wrapper.find('input').simulate('change', {currentTarget: {value: 'Buy Groceries'}})
+        wrapper.find('.todo--button').simulate('click')
+
+        expect(props.addTodo).toHaveBeenCalledWith({text: 'Buy Groceries'})
+    })
+})

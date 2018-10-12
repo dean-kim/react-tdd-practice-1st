d9d90f7f84636fc648ee423f39c8c997b67df526
dean-kim <keunu07@gmail.com>
2018-10-12 11:11:57 +0900

    normalize events with reacts synthetic event system

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,20 +1,32 @@
 import React from 'react';
 
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            currentEvent: '---'
+        }
+        this.update = this.update.bind(this)
+    }
+    update(e) {
+        this.setState({currentEvent: e.type})
+    }
     render() {
         return (
-            <Button>I <Heart /> React</Button>
+            <div>
+                <textarea
+                    onKeyPress={this.update}
+                    onCopy={this.update}
+                    onCut={this.update}
+                    onPaste={this.update}
+                    cols="30"
+                    rows="10" />
+                <h1>{this.state.currentEvent}</h1>
+            </div>
         )
     }
 }
 
-const Button = (props) => <button>{props.children}</button>
-
-class Heart extends React.Component {
-    render() {
-        return <span>&hearts;</span>
-    }
-}
 
 
 export default App
